3d051ee13d000c516e39b30c84010ddac0a43353
delacherie olivier <olivier28111999@hotmail.fr>
2023-06-21 11:36:52 +0200

    fix size square component nbPerson

--- components/NbPerson.tsx
+++ components/NbPerson.tsx
@@ -29,28 +29,28 @@ const NbPerson = () => {
                 <h1 className="title-l mb-6">Tu pars à combien ?</h1>
                 <div className="flex flex-col gap-8">
                     <div className="flex justify-between items-center">
-                        <button className={`text-4xl p-4 border-2 border-neutral-100 rounded-xl aspect-square flex items-center w-12 justify-center ${counts.adults === 0 ? 'text-neutral-500 border-neutral-500' : ''}`} onClick={() => decrement('adults')}>-</button>
+                        <button className={`text-4xl p-4 border-2 border-neutral-100 rounded-xl aspect-square flex items-center w-12 h-12 justify-center ${counts.adults === 0 ? 'text-neutral-500 border-neutral-500' : ''}`} onClick={() => decrement('adults')}>-</button>
                         <div className="flex flex-col items-center text-center">
                             <span className="text-neutral-100 font-bold title-l">{counts.adults}</span>
                             <label className="">Adultes</label>
                         </div>
-                        <button className="text-4xl p-4 border-2 border-neutral-100 rounded-xl aspect-square flex items-center w-12 justify-center" onClick={() => increment('adults')}>+</button>
+                        <button className="text-4xl p-4 border-2 border-neutral-100 rounded-xl aspect-square flex items-center w-12 h-12 justify-center" onClick={() => increment('adults')}>+</button>
                     </div>
                     <div className="flex justify-between items-center">
-                        <button className={`text-4xl p-4 border-2 border-neutral-100 rounded-xl aspect-square flex items-center w-12 justify-center ${counts.children === 0 ? 'text-neutral-500 border-neutral-500' : ''}`} onClick={() => decrement('children')}>-</button>
+                        <button className={`text-4xl p-4 border-2 border-neutral-100 rounded-xl aspect-square flex items-center w-12 h-12 justify-center ${counts.children === 0 ? 'text-neutral-500 border-neutral-500' : ''}`} onClick={() => decrement('children')}>-</button>
                         <div className="flex flex-col items-center text-center">
                             <span className="text-neutral-100 font-bold title-l">{counts.children}</span>
                             <label className="">Enfants</label>
                         </div>
-                        <button className="text-4xl p-4 border-2 border-neutral-100 rounded-xl aspect-square flex items-center w-12 justify-center" onClick={() => increment('children')}>+</button>
+                        <button className="text-4xl p-4 border-2 border-neutral-100 rounded-xl aspect-square flex items-center w-12 h-12 justify-center" onClick={() => increment('children')}>+</button>
                     </div>
                     <div className="flex justify-between items-center">
-                        <button className={`text-4xl p-4 border-2 border-neutral-100 rounded-xl aspect-square flex items-center w-12 justify-center ${counts.babies === 0 ? 'text-neutral-500 border-neutral-500' : ''}`} onClick={() => decrement('babies')}>-</button>
+                        <button className={`text-4xl p-4 border-2 border-neutral-100 rounded-xl aspect-square flex items-center w-12 h-12 justify-center ${counts.babies === 0 ? 'text-neutral-500 border-neutral-500' : ''}`} onClick={() => decrement('babies')}>-</button>
                         <div className="flex flex-col items-center text-center">
                             <span className="text-neutral-100 font-bold title-l">{counts.babies}</span>
                             <label className="">Bébés</label>
                         </div>
-                        <button className="text-4xl p-4 border-2 border-neutral-100 rounded-xl aspect-square flex items-center w-12 justify-center" onClick={() => increment('babies')}>+</button>
+                        <button className="text-4xl p-4 border-2 border-neutral-100 rounded-xl aspect-square flex items-center w-12 h-12 justify-center" onClick={() => increment('babies')}>+</button>
                     </div>
                 </div>
             </div>
